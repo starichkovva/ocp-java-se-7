3267bb5e7ecc8a8157623d100ba3b7e9bbcf69ae
Vadim Starichkov <starichkovva@gmail.com>
2016-03-08 21:37:44 +0300

    some refactoring

--- src/main/java/org/starichkov/java/ocp/Examples.java
+++ src/main/java/org/starichkov/java/ocp/Examples.java
@@ -1,44 +1,70 @@
 package org.starichkov.java.ocp;
 
+import org.starichkov.java.ocp.formatting.CalendarExample;
+import org.starichkov.java.ocp.formatting.DateExample;
+import org.starichkov.java.ocp.formatting.DateFormatExample;
+import org.starichkov.java.ocp.formatting.DateParseExample;
+import org.starichkov.java.ocp.formatting.NumberFormatExample;
+import org.starichkov.java.ocp.formatting.NumberParseExample;
+import org.starichkov.java.ocp.formatting.LocalesExample;
+import org.starichkov.java.ocp.regex.MetaCharacterExample;
+import org.starichkov.java.ocp.regex.QuantifiersExample;
+import org.starichkov.java.ocp.regex.RangesExample;
+import org.starichkov.java.ocp.regex.ScannerRegExExample;
+import org.starichkov.java.ocp.regex.SimpleSearchExample;
+import org.starichkov.java.ocp.tokenizing.ScannerTokenizeExample;
+import org.starichkov.java.ocp.tokenizing.StringSplitTokenizeExample;
+import org.starichkov.java.ocp.tokenizing.StringTokenizerExample;
+
 /**
  * @author Vadim Starichkov
  * @since 28.02.2016 19:30
  */
 public enum Examples {
-    INVALID_EXAMPLE(0),
-    DATE_FORMAT(81),
-    DATE_PARSE(82),
-    NUMBER_FORMAT(83),
-    NUMBER_PARSE(84),
-    DATE(85),
-    CALENDAR(86),
-    LOCALES(87),
-    TOKENIZE_SCANNER(881),
-    TOKENIZE_STRING_SPLIT(882),
-    TOKENIZE_STRING_TOKENIZER(883),
-    REGEX_SIMPLE_SEARCH(891),
-    REGEX_META_CHARACTER(892),
-    REGEX_RANGES(893),
-    REGEX_QUANTIFIERS(894),
-    REGEX_SCANNER(895),
+    DATE_FORMAT(811, new DateFormatExample()),
+    DATE_PARSE(812, new DateParseExample()),
+    NUMBER_FORMAT(821, new NumberFormatExample()),
+    NUMBER_PARSE(822, new NumberParseExample()),
+    DATE(85, new DateExample()),
+    CALENDAR(86, new CalendarExample()),
+    LOCALES(87, new LocalesExample()),
+    TOKENIZE_SCANNER(881, new ScannerTokenizeExample()),
+    TOKENIZE_STRING_SPLIT(882, new StringSplitTokenizeExample()),
+    TOKENIZE_STRING_TOKENIZER(883, new StringTokenizerExample()),
+    REGEX_SIMPLE_SEARCH(891, new SimpleSearchExample()),
+    REGEX_META_CHARACTER(892, new MetaCharacterExample()),
+    REGEX_RANGES(893, new RangesExample()),
+    REGEX_QUANTIFIERS(894, new QuantifiersExample()),
+    REGEX_SCANNER(895, new ScannerRegExExample()),
     ;
 
     private final int code;
+    private final RunnableExample example;
 
-    Examples(int code) {
+    Examples(int code, RunnableExample example) {
         this.code = code;
+        this.example = example;
     }
 
     public int getCode() {
         return code;
     }
 
-    public static Examples findByCode(int code) {
+    public RunnableExample getExample() {
+        return example;
+    }
+
+    public static RunnableExample findByCode(int code) {
         for (Examples example : values()) {
             if (example.getCode() == code) {
-                return example;
+                return example.getExample();
             }
         }
-        return INVALID_EXAMPLE;
+        return new RunnableExample() {
+            @Override
+            public void run() {
+                System.out.println("Unknown example requested.");
+            }
+        };
     }
 }
--- src/main/java/org/starichkov/java/ocp/Main.java
+++ src/main/java/org/starichkov/java/ocp/Main.java
@@ -1,84 +1,15 @@
 package org.starichkov.java.ocp;
 
-import org.starichkov.java.ocp.formatting.CalendarExample;
-import org.starichkov.java.ocp.formatting.DateExample;
-import org.starichkov.java.ocp.formatting.DateFormatExample;
-import org.starichkov.java.ocp.formatting.DateParseExample;
-import org.starichkov.java.ocp.formatting.NumberFormatExample;
-import org.starichkov.java.ocp.formatting.NumberParseExample;
-import org.starichkov.java.ocp.formatting.LocalesExample;
-import org.starichkov.java.ocp.regex.MetaCharacterExample;
-import org.starichkov.java.ocp.regex.QuantifiersExample;
-import org.starichkov.java.ocp.regex.RangesExample;
-import org.starichkov.java.ocp.regex.ScannerRegExExample;
-import org.starichkov.java.ocp.regex.SimpleSearchExample;
-import org.starichkov.java.ocp.tokenizing.ScannerTokenizeExample;
-import org.starichkov.java.ocp.tokenizing.StringSplitTokenizeExample;
-import org.starichkov.java.ocp.tokenizing.StringTokenizerExample;
-
 /**
  * @author Vadim Starichkov
  * @since 28.02.2016 19:13
  */
 public final class Main {
     public static void main(String[] args) {
-        RunnableExample runnableExample = null;
         try {
             int choice = Integer.parseInt(args[0]);
-            switch (Examples.findByCode(choice)) {
-                case DATE_FORMAT:
-                    runnableExample = new DateFormatExample();
-                    break;
-                case DATE_PARSE:
-                    runnableExample = new DateParseExample();
-                    break;
-                case NUMBER_FORMAT:
-                    runnableExample = new NumberFormatExample();
-                    break;
-                case NUMBER_PARSE:
-                    runnableExample = new NumberParseExample();
-                    break;
-                case DATE:
-                    runnableExample = new DateExample();
-                    break;
-                case CALENDAR:
-                    runnableExample = new CalendarExample();
-                    break;
-                case LOCALES:
-                    runnableExample = new LocalesExample();
-                    break;
-                case TOKENIZE_SCANNER:
-                    runnableExample = new ScannerTokenizeExample();
-                    break;
-                case TOKENIZE_STRING_SPLIT:
-                    runnableExample = new StringSplitTokenizeExample();
-                    break;
-                case TOKENIZE_STRING_TOKENIZER:
-                    runnableExample = new StringTokenizerExample();
-                    break;
-                case REGEX_SIMPLE_SEARCH:
-                    runnableExample = new SimpleSearchExample();
-                    break;
-                case REGEX_META_CHARACTER:
-                    runnableExample = new MetaCharacterExample();
-                    break;
-                case REGEX_RANGES:
-                    runnableExample = new RangesExample();
-                    break;
-                case REGEX_QUANTIFIERS:
-                    runnableExample = new QuantifiersExample();
-                    break;
-                case REGEX_SCANNER:
-                    runnableExample = new ScannerRegExExample();
-                    break;
-                case INVALID_EXAMPLE:
-                default:
-                    System.out.println("Unknown example requested.");
-                    break;
-            }
-            if (runnableExample != null) {
-                runnableExample.run();
-            }
+            RunnableExample runnableExample = Examples.findByCode(choice);
+            runnableExample.run();
         } catch (NumberFormatException e) {
             e.printStackTrace();
         }
