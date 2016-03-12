a634d07a90294e3c7df832985be8b6d6c7c56b40
Vadim Starichkov <starichkovva@gmail.com>
2016-03-12 20:01:01 +0300

    Updated PrintStream format example

--- src/main/java/org/starichkov/java/ocp/formatting/PrintStreamFormatExample.java
+++ src/main/java/org/starichkov/java/ocp/formatting/PrintStreamFormatExample.java
@@ -14,10 +14,11 @@ public class PrintStreamFormatExample implements RunnableExample {
     public void run() {
         int i1 = -123;
         int i2 = 12345;
-        System.out.printf(">%1$(7d< \n", i1);
-        System.out.printf(">%0,7d< \n", i2);
-        System.out.format(">%+-7d< \n", i2);
-        System.out.printf(">%2$b + %1$5d< \n", i1, false);
+        System.out.printf("| %1$(10d | \n", i1);
+        System.out.format("| %+-10d | \n", i1);
+        System.out.format("| %+-10d | \n", i2);
+        System.out.printf("| %0,10d | \n", i2);
+        System.out.printf("| %2$b + %1$5d | \n", i1, false);
 
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(new Date());
